3c6d9cf75085ff9dc16b739227da4d644f1cc26f
causadol <92407111+causadol@users.noreply.github.com>
2021-10-22 22:30:56 -0500

    Proyecto ICESI completo
    primer repositorio completo ICESI

--- app.py
+++ app.py
@@ -1,1 +1,61 @@
-print ("Hola Mundo Gaviria")
+from flask import Flask, render_template, request
+import controller
+
+app= Flask(__name__)
+
+@app.route('/')
+def login():
+    resultado='invisible'
+    return render_template('login.html',resultado=resultado)
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/estudiantes')
+def estudiantes():
+    return render_template('estudiantes.html')
+
+@app.route('/docentes')
+def docentes():
+    return render_template('docentes.html')
+
+@app.route('/materias')
+def materias():
+    return render_template('materias.html')
+
+@app.route('/notas')
+def notas():
+    return render_template('notas.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/sesion', methods=('GET','POST'))
+def sesion():    
+    if request.method == 'POST':
+        usuario=request.form['user']
+        clave=request.form['pass']        
+        # inicio base de datos
+        response=controller.Ingreso(usuario,clave)
+        if response:
+            resultado=response[0]
+            option=['DOCENTE','DOCENTEE','ADMIN']
+            if resultado=='ESTUDIANTE':
+                #return response
+                return render_template('home.html',resultado= resultado)
+            elif resultado=='DOCENTE':
+                return render_template('prueba.html',resultado= resultado,option=option)
+            else:
+                return render_template('prueba.html',resultado= resultado)
+        else:
+            resultado='visible'
+            return render_template('login.html',resultado= resultado)        
+            
+    else:
+        return ('Usuario y contraseña mala')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
---
+++ controller.py
@@ -0,0 +1,20 @@
+import sqlite3
+from sqlite3 import Error
+import mysql.connector
+
+def Conectar():
+    # Establecido una ruta fisica para la BDD
+    #conexion= sqlite3.connect("D:\\DATABASES\\bdd.db")
+    conexion= sqlite3.connect("c:\\db_icesi\\ciclo3db.db")
+    #conexion=mysql.connector()
+    return conexion
+
+def Ingreso(usuario, password):    
+    conn= Conectar()
+    cursor= conn.execute("SELECT roles.nombre FROM login INNER JOIN `user` ON (login.id_user=`user`.id)	INNER JOIN roles ON (`user`.id_rol= roles.id) WHERE login.user=? AND login.pass=?;",(usuario,password))
+    #for item in cursor:
+    return cursor.fetchone() #item
+    conn.close()        
+    
+   
+
